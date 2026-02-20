5b414c843f92542537cfbe7be82ea3fe4e9ee400
kinsa24 <muhkinsa081924@gmail.com>
2026-02-20 23:26:00 +0800

    update src

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,18 +1,5 @@
 import {
-  bankingapp,
-  dicegame,
-  gifgenerator,
-  marveldc,
-  retrogame,
   todolist,
-  travel,
-  wedding,
-  clicare,
-  pokemon,
-  landingpage,
-  timetracker,
-  tipCalculator,
-  fylo,
   calculator1,
   moviebox,
   diskominfo,
@@ -20,109 +7,6 @@ import {
 } from "../assets/projectsimages";
 
 export const projects = [
-  // {
-  //   id: 1,
-  //   title: "Booking App",
-  //   url: "https://bookingtravelreact.netlify.app",
-  //   description:
-  //     "App About travel Booking in Wonderful destinations in Argentina.",
-  //   image: travel,
-  // },
-  // {
-  //   id: 2,
-  //   title: "Banking App",
-  //   url: "https://banking-app-gap.netlify.app/",
-  //   description: "App About Banking Balance, transfer and Loan with auth.",
-  //   image: bankingapp,
-  // },
-  // {
-  //   id: 3,
-  //   title: "Roll the dice",
-  //   url: "https://jst-dice-game.netlify.app/",
-  //   description: "Funny Game 1vs1. The first one to reach 20 points wins.",
-  //   image: dicegame,
-  // },
-  // {
-  //   id: 4,
-  //   title: "Gif Generator",
-  //   url: "https://gif-generator-gap.netlify.app/",
-  //   description: "Simple App to searh funnies gifs",
-  //   image: gifgenerator,
-  // },
-  // {
-  //   id: 5,
-  //   title: "Guess Number",
-  //   url: "https://guessnumber-jst.netlify.app/",
-  //   description: "Inspire on Atari, funny game about guess the Number",
-  //   image: retrogame,
-  // },
-  // {
-  //   id: 6,
-  //   title: "Marvel-DC Cards",
-  //   url: "https://comics-heroes-marveldc.netlify.app",
-  //   description: "Nice Web App about Heroes and their alter-ego characters",
-  //   image: marveldc,
-  // },
-  // {
-  //   id: 7,
-  //   title: "Wedding invitation",
-  //   url: "https://wedding-german-nati.netlify.app",
-  //   description: "Nice Interactive Wedding Invitation, Only Mobile Design",
-  //   image: wedding,
-  // },
-  // {
-  //   id: 8,
-  //   title: "TO DO List",
-  //   url: "https://todolist-app-gap.netlify.app/",
-  //   description:
-  //     "Simple TODO List for practicing Manipulation of the DOM and Logic",
-  //   image: todolist,
-  // },
-  // {
-  //   id: 9,
-  //   title: "Shopping Cart",
-  //   url: "https://clicare.io",
-  //   description:
-  //     "Shopping cart made for Marcketplace Clicare this company sells healthcare products",
-  //   image: clicare,
-  // },
-  // {
-  //   id: 10,
-  //   title: "Pokemon App",
-  //   url: "https://pokemon-appdev.netlify.app/",
-  //   description: "Funny App about pokemon characters and their evolutions",
-  //   image: pokemon,
-  // },
-  // {
-  //   id: 11,
-  //   title: "Landing Page",
-  //   url: "https://landingpage-frontendmentor-challenge.netlify.app/",
-  //   description: "Responsive Landing page with fancy design",
-  //   image: landingpage,
-  // },
-  // {
-  //   id: 12,
-  //   title: "Time Tracking Dashboard",
-  //   url: "https://frontendmentor-challenge21.netlify.app/",
-  //   description: "Responsive Time Tracking Dashboard with animation",
-  //   image: timetracker,
-  // },
-  // {
-  //   id: 13,
-  //   title: "Tip Calculator",
-  //   url: "https://frontendmentor-challenge22-germanp007.netlify.app/",
-  //   description:
-  //     "Tip Calculator App, it uses to divide Bill between people based on Tips percentage.",
-  //   image: tipCalculator,
-  // },
-  // {
-  //   id: 14,
-  //   title: "Fylo Landing Page",
-  //   url: "https://frontendmentor-challenge24.netlify.app/",
-  //   description: "A Landing with responsive design.",
-  //   image: fylo,
-  // },
-
   {
     id: 1,
     title: "Todolist App",
